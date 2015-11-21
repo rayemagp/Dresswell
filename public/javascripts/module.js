ce02e65499338467600e332a527983689de12d96
Rayen Magpantay <raye.magp@gmail.com>
2015-11-22 01:34:18 +1100

    first deploy

---
+++ public/javascripts/module.js
@@ -0,0 +1,47 @@
+var app = angular.module('thisApp', []);
+
+app.controller('thisController', [
+    '$scope', '$http',
+    function($scope, $http) {
+
+        $scope.show = false;
+        $scope.generate = false;
+
+        $scope.recommendations = [{
+            "Outfit1":{
+
+                "0":{
+                    brand: "UNIQLO",
+                    name: "Easy Care Check Long Sleeve",
+                    price: 29.90,
+                    photo: "http://d2h2vnfmmg5sct.cloudfront.net/catalog/product/large_image/06_133497.jpg",
+                    link: "http://www.uniqlo.com/au/store/men-easy-care-check-long-sleeve-shirt-1334970006.html"
+                },
+                "1":{
+                    brand: "UNIQLO",
+                    name: "Stretch Slim Fit Tapered Blue",
+                    price: 29.90,
+                    photo: "http://d2h2vnfmmg5sct.cloudfront.net/catalog/product/large_image/66_143983.jpg",
+                    link: "http://www.uniqlo.com/au/store/men-stretch-slim-fit-tapered-jeans-1439830032.html"
+                }
+            },
+            "Outfit2":{
+                "0":{
+                    brand: "UNIQLR",
+                    name: "Easy Care Check Long Sleeve",
+                    price: 29.90,
+                    photo: "http://d2h2vnfmmg5sct.cloudfront.net/catalog/product/large_image/06_133497.jpg",
+                    link: "http://www.uniqlo.com/au/store/men-easy-care-check-long-sleeve-shirt-1334970006.html"
+                },
+                "1":{
+                    brand: "UNIQLR",
+                    name: "Stretch Slim Fit Tapered Blue",
+                    price: 29.90,
+                    photo: "http://d2h2vnfmmg5sct.cloudfront.net/catalog/product/large_image/66_143983.jpg",
+                    link: "http://www.uniqlo.com/au/store/men-stretch-slim-fit-tapered-jeans-1439830032.html"
+                }
+            }
+
+        }];
+
+}]);
